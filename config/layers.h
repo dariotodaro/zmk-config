8151caa0fa12d30f8e36191716898aba4629fef9
Todd <dario.todaro1@gmail.com>
2026-09-25 12:25:37 +0200

    try to move layer definitions to own header

---
+++ config/layers.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define BASE         0
+#define MOUSE        1
+#define SCRL         2
+#define LOWER        3
+#define RAISE        4
+#define FUNC         5
+#define GAME         6
+#define G_N          7
+#define ADJUST       8
